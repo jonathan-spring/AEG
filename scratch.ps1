1bd9b8de03894cc04d7c82e448858e11b90b29ac
Jonathan-Spring <jspring@aegvision.com>
2026-02-01 20:07:14 -0600

    Moving unfinished functions to a scratch file

---
+++ scratch.ps1
@@ -0,0 +1,48 @@
+# Needs work
+# function Install-Officemate {
+#      param (
+#         $version 
+#     )
+#     $Officemate = $Software.OfficeMate
+#     # Validate version
+#     if ([string]::IsNullOrWhiteSpace($version)) {
+#         throw "No version specified. Please provide a valid version number (e.g., Install-OMate -version '15.3.0.4578')."
+#     }
+
+#     $baseUrl = $Officemate.BaseUrl
+
+#     $fileName = $Officemate.exeName
+#     $downloadUrl = "$baseUrl/OmWorkstationInstaller$version/$fileName"
+
+#     # Install-PackageFromURL
+    
+    
+
+#     # Create output directory if it doesn't exist
+#     if (-not (Test-Path $outputFolder)) {
+#         New-Item -ItemType Directory -Path $outputFolder | Out-Null
+#     }
+
+    
+#     # Creates the directory for the .ini file
+#     $targetPath = "C:\ProgramData\Eyefinity\OfficeMate\Settings"
+
+#     # Check if the path exists
+#     if (Test-Path $targetPath) {
+#         Write-Host "Path already exists: $targetPath"
+#     } else {
+#         try {
+#             # Create the full directory structure if it doesn't exist
+#             New-Item -Path $targetPath -ItemType Directory -Force | Out-Null
+#             Write-Host "Created path: $targetPath"
+#         } catch {
+#             Write-Host "Failed to create path: $($_.Exception.Message)" -ForegroundColor Red
+#         }
+#     }
+#     if (Test-Path $outputFile) {
+#     Write-Host "Starting installation..."
+#     Start-Process -FilePath $outputFile -Wait
+#     Write-Host "Installation completed (or installer closed)."
+#     }
+
+# }
